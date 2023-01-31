66fc4567adab96a669d9be73a3420c350b4ac60d
renatawojcik <renatamalota@gmail.com>
2023-01-31 16:43:17 +0100

    Praca domowa dzień 4, wersja 2023

---
+++ OdPoczatku/Program.cs
@@ -0,0 +1,35 @@
+﻿
+using Microsoft.Win32.SafeHandles;
+internal class Program
+{
+    private static void Main(string[] args)
+{
+    string name = "Marek";
+    string name2 = "Ania";
+    string name3 = "Ewa";
+    char female = 'K';
+    char male = 'M';
+    int ageOfMark = 16;
+    int ageAnn = 29;
+    int ageEwa = 33;
+    bool woman = true;
+    bool ageUnder30 = ageAnn < 30;
+    bool mature = ageOfMark > 18;
+
+    if (ageUnder30 && woman)
+    {
+        Console.WriteLine("Ania jest kobietą poniżej 30 lat.");
+    }
+
+    if (mature == true)
+    {
+        Console.WriteLine("Marek jest pełnoletni.");
+    }
+    else
+    {
+        Console.WriteLine("Marek jest niepełnoletni.");
+    }
+
+    Console.WriteLine(name3 + " ma lat " + ageEwa + ".");
+}
+}
