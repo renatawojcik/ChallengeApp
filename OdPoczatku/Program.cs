841dd4d90c6d3581a1b4c08e3cd271fea689b656
renatawojcik <renatamalota@gmail.com>
2023-01-31 20:25:01 +0100

    Zadanie Domowe dzień 5 [2023]

--- OdPoczatku/Program.cs
+++ OdPoczatku/Program.cs
@@ -3,33 +3,76 @@ using Microsoft.Win32.SafeHandles;
 internal class Program
 {
     private static void Main(string[] args)
-{
-    string name = "Marek";
-    string name2 = "Ania";
-    string name3 = "Ewa";
-    char female = 'K';
-    char male = 'M';
-    int ageOfMark = 16;
-    int ageAnn = 29;
-    int ageEwa = 33;
-    bool woman = true;
-    bool ageUnder30 = ageAnn < 30;
-    bool mature = ageOfMark > 18;
-
-    if (ageUnder30 && woman)
     {
-        Console.WriteLine("Ania jest kobietą poniżej 30 lat.");
-    }
+        int number = 5657779;
+        string numberToString = number.ToString();
+        char[] letters = numberToString.ToCharArray();
+     
 
-    if (mature == true)
-    {
-        Console.WriteLine("Marek jest pełnoletni.");
-    }
-    else
-    {
-        Console.WriteLine("Marek jest niepełnoletni.");
-    }
+        int counter0 = 0;
+        int counter1 = 0;
+        int counter2 = 0;
+        int counter3 = 0;
+        int counter4 = 0;
+        int counter5 = 0;
+        int counter6 = 0;
+        int counter7 = 0;
+        int counter8 = 0;
+        int counter9 = 0;
+      
 
-    Console.WriteLine(name3 + " ma lat " + ageEwa + ".");
-}
+        foreach (char letter in letters)
+        { if (letter == '0')
+            { counter0++; }
+            
+        else if (letter == '1')
+            { counter1++; }
+            else if (letter == '2')
+            { counter2++; }
+            else if (letter == '3')
+            { counter3++; }
+            else if (letter == '4')
+            { counter4++; }
+            else if (letter == '5')
+            { counter5++; }
+            else if (letter == '6')
+            { counter6++; }
+            else if (letter == '7')
+            { counter7++; }
+            else if (letter == '8')
+            { counter8++; }
+            else if (letter == '9')
+            { counter9++; }
+        }
+
+        Console.WriteLine("Ilość cyfr 0 ");
+        Console.WriteLine(counter0);
+
+        Console.WriteLine("Ilość cyfr 1 ");
+        Console.WriteLine(counter1);
+
+        Console.WriteLine("Ilość cyfr 2 ");
+        Console.WriteLine(counter2);
+
+        Console.WriteLine("Ilość cyfr 3 ");
+        Console.WriteLine(counter3);
+
+        Console.WriteLine("Ilość cyfr 4 ");
+        Console.WriteLine(counter4);
+
+        Console.WriteLine("Ilość cyfr 5 ");
+        Console.WriteLine(counter5);
+
+        Console.WriteLine("Ilość cyfr 6 ");
+        Console.WriteLine(counter6);
+
+        Console.WriteLine("Ilość cyfr 7 ");
+        Console.WriteLine(counter7);
+        
+        Console.WriteLine("Ilość cyfr 8 ");
+        Console.WriteLine(counter8);
+
+        Console.WriteLine("Ilość cyfr 9 ");
+        Console.WriteLine(counter9);
+    }
 }
